460ee97fd2fb129f75d25bad28441b27262c6bb8
Maryia Rotar <rotarmaryia@gmail.com>
2023-01-12 04:27:29 +0200

    "-"

---
+++ src/main/java/com/rotar/PhotoEditorWeb/Models/Filling.java
@@ -0,0 +1,13 @@
+package com.rotar.PhotoEditorWeb.Models;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public enum Filling {
+
+    HORIZONTAL,
+    VERTICAL
+
+}
